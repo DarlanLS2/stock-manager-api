30fb3353cdd0c01c5a9c7d59aa74ae46c3da782d
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-01-10 08:30:09 -0300

    feat: criar userRoutes

---
+++ src/routes/userRoute.js
@@ -0,0 +1,22 @@
+export class ProductRoute {
+  constructor(server, userController) {
+    this.controller = userController
+    this.server = server;
+  }
+
+  create() {
+    this.server.get("/user/:email", async (req, res) => {
+      await this.controller.getUserByEmail(req, res);
+    });
+    this.server.post("/user", async (req, res) => {
+      await this.controller.register(req, res);
+    });
+    this.server.put("/user/:id", async (req, res) => {
+      await this.controller.update(req, res);
+    })
+    this.server.delete("/user/:id", async (req, res) => {
+      await this.controller.delete(req, res);
+    });
+  }
+}
+
