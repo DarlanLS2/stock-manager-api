5bd8014139898e5586794cc4818428e0452952a0
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-01-15 16:14:39 -0300

    feat(authMidleware): implementar controle de erros

--- src/authentication/authMidleware.js
+++ src/authentication/authMidleware.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken"
+
 export function authMidleware(req, res, next) {
   const authHeader = req.headers.authorization;
 
@@ -5,9 +7,25 @@ export function authMidleware(req, res, next) {
     return res.status(401).json({ message: "Token não informado" })
   }
 
-  if (authHeader != "mock") {
-    return res.status(401).json({ message: "Token invalido" })
+  const parts = authHeader.split(" ");
+
+  if (parts.lengh !== 2) {
+    return res.status(401).json({ message: "Token mal formatado" })
   }
 
-  next()
+  const [scheme, token] = parts
+
+  if (scheme !== "Bearer") {
+    return res.status(401).json({ message: "Token mal formatado" })
+  }
+
+  try {
+    const decoded = jwt.verify(token, "segredo_mockado")
+
+    req.userId = decoded.sub;
+
+    next()
+  } catch (error) {
+    return res.status(401).json({ error: "Token invalido ou expirado" })
+  }
 }
