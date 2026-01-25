6f6e091722c929c5582b99cb3cae328200e4d7aa
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-01-25 10:44:39 -0300

    fix(models): correct userModel and productModel imports

--- src/database/models/userModel.js
+++ src/database/models/userModel.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "./sequelize.js";
+import { sequelize } from "../sequelize.js";
 
 export const User = sequelize.define("user", {
   email: {
