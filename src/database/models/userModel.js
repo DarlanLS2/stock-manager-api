35e98942e1241a12477dbfa6fe36f7a54639f247
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-02-08 13:12:18 -0300

    feat(user-model): add unique constraint to email field

--- src/database/models/userModel.js
+++ src/database/models/userModel.js
@@ -4,6 +4,7 @@ import { sequelize } from "../sequelize.js";
 export const User = sequelize.define("user", {
   email: {
     type: DataTypes.STRING,
+    unique: true,
     allowNull: false,
   },
   passWordHash: {
