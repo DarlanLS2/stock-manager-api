eed3f76d4b154560394ded7f314552c0b9c9ae1a
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-02-16 17:36:01 -0300

    refactor(entities): migrate Product to TypeScript

--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -1,14 +1,28 @@
 import { ValidationError } from "../errors/ValidationError.js"
 
+type Fields = {
+  id?: string,
+  name: string,
+  price: string,
+  quantity: string,
+  description: string
+}
+
 export class Product {
-  constructor(fields) {
+  id: string | undefined
+  name: string
+  price: string
+  quantity: string
+  description: string
+
+  constructor(fields: Fields) {
     this.#validateName(fields.name);
     this.#validatePrice(fields.price);
     this.#validateQuantity(fields.quantity);
     this.#validateDescription(fields.description);
 
-    if (fields.id == null) {
-      this.id = null;
+    if (fields.id == undefined) {
+      this.id = undefined;
     } else {
       this.id = fields.id;
     }
@@ -18,49 +32,32 @@ export class Product {
     this.description = fields.description;
   }
 
-  #validateName(name) {
+  #validateName(name: string) {
     const regex = /^(?=.{3,100}$)[A-Za-z0-9À-ÖØ-öø-ÿ]+(?:[ _-][A-Za-z0-9À-ÖØ-öø-ÿ]+)*$/;
-
-    if (name == null) {
-      throw new ValidationError("name", "required");
-    }
 
     if (!regex.test(name)) {
       throw new ValidationError("name", "invalid_format");
     }
   }
 
-  #validatePrice(price) {
+  #validatePrice(price: string) {
     const regex = /^(0|[1-9]\d*)(\.\d{1,2})?$/;
-
-    if (price == null) {
-      throw new ValidationError("price", "required");
-    } 
 
     if (!regex.test(price)) {
       throw new ValidationError("price", "invalid_format");
     }
   }
 
-  
-  #validateQuantity(quantity) {
+  #validateQuantity(quantity: string) {
     const regex = /^(0|[1-9]\d*)$/;
-
-    if (quantity == null) {
-      throw new ValidationError("quantity", "required");
-    } 
 
     if (!regex.test(quantity)) {
       throw new ValidationError("quantity", "invalid_format");
     }
   }
 
-  #validateDescription(description) {
+  #validateDescription(description: string) {
     const regex = /^(?=.{10,500}$)(?!\s*$)[A-Za-z0-9À-ÖØ-öø-ÿ\s.,;:!?()'"%\-_/]+$/
-
-    if (description == null) {
-      throw new ValidationError("description", "required");
-    }
 
     if (!regex.test(description)) {
       throw new ValidationError("description", "invalid_format")
