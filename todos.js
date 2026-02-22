72573be820a56a669b679d82e493035b028dd452
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-02-22 23:01:31 +0000

    Delete todos.js

--- todos.js
+++
@@ -1,3 +0,0 @@
-// TODO: Renomear User do userModel do database para UserModel
-// TODO: Fazer os testes de integração com SuperTest
-// TODO: Ideia de regra de negocio: tranformar input de preço para real (com virgula)
